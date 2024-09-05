7b2e786cfc36567d8794d1c9125a467f3d023097
anastassaoui <anass.tassaoui@gmail.com>
2024-09-05 12:25:54 +0000

    fixed the requirements.txt
    i hope i fixed it at least hhh 12.25

--- database.py
+++ database.py
@@ -1,9 +1,17 @@
 from flask import g
 import sqlite3
+import os
 
 def connect_db():
-    # Update the path to point to your database file
-    sql = sqlite3.connect('C:\\Users\\ANAS\\Desktop\\The-Abs-Project\\absence.db')
+    # Determine the path to the database based on the environment
+    if os.getenv('FLASK_ENV') == 'production':
+        # Use a relative path that will work on Render
+        db_path = 'absence.db'
+    else:
+        # Local development path
+        db_path = 'C:\\Users\\ANAS\\Desktop\\The-Abs-Project\\absence.db'
+    
+    sql = sqlite3.connect(db_path)
     sql.row_factory = sqlite3.Row
     return sql
 
